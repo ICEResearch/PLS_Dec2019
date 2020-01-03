6bafe26da2e8a28eb3d534ec4c5b39cdde700c12
Benjamin Jensen <benjjensen@Live.com>
2020-01-03 13:52:12 -0700

    Added a Rx file for the donut (capacity vs distance) subproject

---
+++ DonutCapacity/donutRx.m
@@ -0,0 +1,56 @@
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+% Function used to gather data for the Capacity vs Distance project
+% This connects to a radio, and then gathers data continuously between two
+% times. The data and time stamps are stored and then saved at the end.
+% 
+% Made Jan 3, 2020
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+
+clear; close all; clc;
+
+%%%%% USER INPUT %%%%%
+startTime = "03-Jan-2020 13:41:00";
+endTime = "03-Jan-2020 13:41:30";
+arrayName = 'testing';
+%%%%%%%%%%%%%%%%%%%%%%
+dataArray = zeros(6400, 2048); % 6400 ~= 1 minute
+
+
+    % Radio setup 
+plutoradiosetup();
+rx = sdrrx('Pluto');
+rx.RadioID = 'usb:0';
+rx.CenterFrequency = 1250e6;
+rx.BasebandSampleRate = 20e6;
+rx.SamplesPerFrame = 32*64;
+rx.OutputDataType = 'double';
+rx.ShowAdvancedProperties = true;
+
+    % Ignore data to account for buffer
+for ignore = 1:10
+    junk = rx();
+end
+
+    % Wait to start until the correct time
+while string(datetime) <= startTime
+end
+
+beep
+disp("Starting at " + string(datetime));
+
+count = 1;
+while datetime < endTime
+    dataArray(count,:) = rx();
+    timeArray(count) = datetime;
+    count = count + 1;
+end
+
+beep
+
+dataName = string(arrayName) + "_data";
+timeName = string(arrayName) + "_time";
+eval(sprintf("%s = dataArray;",dataName));
+eval(sprintf("%s = timeArray;",timeName));
+
+save(dataName + '.mat',dataName); % Saves data array with custom name
+save(timeName + '.mat',timeName); % Saves data array with custom name
